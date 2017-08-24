2272d3a5803baf399cdf80032ea82c0de31188f6
Adam Stirtan <adamstirtan@gmail.com>
2017-08-24 09:48:11 -0400

    Made talk a bit more easier to read

--- Slacker/Triggers/Talk/TalkTrigger.cs
+++ Slacker/Triggers/Talk/TalkTrigger.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 using Markov;
@@ -14,6 +16,18 @@ namespace Slacker.Triggers.Talk
 {
     public class TalkTrigger : ITrigger
     {
+        private readonly List<string> _messageStarts = new List<string>
+        {
+            "If I were #USER#, I'd probably say:\n",
+            "Oh that son of a bitch, they'd say something like:\r",
+            "Watch this it's going sound just like #USER#:\n",
+            "#USER# is dumb, I guess he'd say:\n",
+            "That's easy, #USER# would say:\n",
+            "He always says shit like:\n",
+            "Pretending to be a terrible person is easy, watch:\n",
+            "That shitface would be like:\n"
+        };
+
         public string Name => "!talk";
 
         public Task Execute(ITriggerArguments arguments, ISlackConnection connection, SlackChatHub chatHub, SlackerContext context)
@@ -50,13 +64,24 @@ namespace Slacker.Triggers.Talk
                 chain.Add(message.Content.Split(new[] { ' ' }), 1);
             }
 
+            var random = new Random(DateTime.UtcNow.Millisecond);
+
+            var stringBuilder = new StringBuilder(GetRandomMessageStart(user, random));
+
+            stringBuilder.Append(">" + string.Join(" ", chain.Chain(random)));
+
             connection.Say(new BotMessage
             {
                 ChatHub = chatHub,
-                Text = string.Join(" ", chain.Chain(new Random(DateTime.UtcNow.Millisecond)))
+                Text = stringBuilder.ToString()
             });
 
             return Task.CompletedTask;
+        }
+
+        private string GetRandomMessageStart(string user, Random random)
+        {
+            return _messageStarts[random.Next(_messageStarts.Count)].Replace("#USER#", user);
         }
     }
 }
